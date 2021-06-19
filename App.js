2a5fbcd71aab9a804c3f30f72161d91f2bf9b19b
Sundar <sundar-stack>
2021-06-19 15:57:11 +0530

    initalized the project

---
+++ App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+
+const App = ()=>{
+  return (
+    <View>
+      <Text>Hello World</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+});
+
+export default App;
